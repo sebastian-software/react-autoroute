684be7036dd835d20e180630ba44af105eb24b47
Sebastian Werner <s.werner@sebastian-software.de>
2022-12-09 15:40:16 +0100

    Adding layout to posts example

---
+++ examples/react-router/src/routes/posts/_.tsx
@@ -0,0 +1,5 @@
+import { Outlet } from "react-router-dom";
+
+export default function PostsLayout() {
+  return <div style={{ background: "#f2f2f2", padding: "20px", borderRadius: "8px"}}><Outlet/></div>
+}
