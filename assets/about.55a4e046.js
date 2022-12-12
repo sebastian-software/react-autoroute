15437e1398f3359aab3a4e2cd9fb5ac7a4a82d38
swernerx <s.werner@sebastian-software.de>
2022-12-12 11:54:19 +0000

    Deploying to gh-pages from @ sebastian-software/react-autoroute@bc628a4c802d6a265cd1d46551af06abf49da58a 🚀

---
+++ assets/about.55a4e046.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index.06e4ab1d.js";function r(){return t("h1",{children:"About Page"})}export{r as default};
---
+++ assets/imprint.b673bb8c.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index.06e4ab1d.js";function n(){return r("h1",{children:"Imprint Page"})}export{n as default};
