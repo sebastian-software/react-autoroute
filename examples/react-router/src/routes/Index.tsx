8d107823b5d755018f60cac32be5617c051568e1
Sebastian Werner <s.werner@sebastian-software.de>
2023-02-02 14:04:46 +0100

    feat: added support and example for loader method

--- examples/react-router/src/routes/Index.tsx
+++ examples/react-router/src/routes/Index.tsx
@@ -1,9 +1,32 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLoaderData } from "react-router-dom";
+
+async function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+export interface LoaderResult {
+  firstName: string
+  name: string
+}
+
+export async function Loader(): Promise<LoaderResult> {
+  // This could be any `fetch()` which loads data from a remote
+  await sleep(500)
+
+  return {
+    firstName: "Gregory",
+    name: "Schmidt"
+  }
+}
 
 export default function Home() {
+  // As of React Router 6.8.0 there is no generic parameter to `useLoaderData`
+  // See also: https://github.com/remix-run/react-router/discussions/9854
+  const x = useLoaderData() as LoaderResult
   return (
     <>
       <h1>Home Page (with Outlet to render children)</h1>
+      <p>Howdy {x.firstName} {x.name}</p>
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="about">About</Link></li>
--- src/loader.tsx
+++ src/loader.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from "react";
-import type { ActionFunction, LoaderFunction } from 'react-router-dom'
+import type { ActionFunction, LoaderFunction, LoaderFunctionArgs } from 'react-router-dom'
 
 export type ElementFactory = () => JSX.Element
 
@@ -8,11 +8,15 @@ export type ReactRouterImportMap = Record<string, () => Promise<ReactRouterRoute
 
 export function modulesToLazyRouteObjects(imports: ReactRouterImportMap, root: string) {
   const routeConfig: Record<string, any> = {}
-  for (var fileName in imports) {
+  for (const fileName in imports) {
     const LazyElement = lazy(imports[fileName])
 
     routeConfig[fileName.replace(root, "")] = {
       element: <Suspense fallback={null} children={<LazyElement />} />,
+      loader: async (params: LoaderFunctionArgs) => {
+        const callback = await imports[fileName]().then((module) => module.Loader)
+        return callback ? callback(params) : null
+      }
     }
   }
 
