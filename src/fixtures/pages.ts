480375cdf9c66cf4ba7eb5a2c14ec9f4ec3b5d84
Sebastian Werner <s.werner@sebastian-software.de>
2023-02-03 21:43:02 +0100

    chore: auto formatted

--- src/fixtures/pages.ts
+++ src/fixtures/pages.ts
@@ -1,40 +1,46 @@
 export const rootLayout = {
-  '_.tsx': { element: '<LayoutRoot />', errorElement: '<PageRootError />' },
+  "_.tsx": { element: "<LayoutRoot />", errorElement: "<PageRootError />" }
 }
 
 export const rootPages = {
-  'about.tsx': { element: '<PageAbout />' },
-  'index.tsx': { element: '<PageRoot />' },
-  '404.tsx': { element: '<NotFound />' },
+  "about.tsx": { element: "<PageAbout />" },
+  "index.tsx": { element: "<PageRoot />" },
+  "404.tsx": { element: "<NotFound />" }
 }
 
 export const rootPagesCamelCase = {
-  'AboutPage.tsx': { element: '<PageAbout />' },
-  'Index.tsx': { element: '<PageRoot />' },
-  '404.tsx': { element: '<NotFound />' },
+  "AboutPage.tsx": { element: "<PageAbout />" },
+  "Index.tsx": { element: "<PageRoot />" },
+  "404.tsx": { element: "<NotFound />" }
 }
 
 export const postsPagesWithLayout = {
-  'posts.archive.tsx': { element: '<PagePostsArchive />' },
-  'posts/_.tsx': { element: '<LayoutPosts />', errorElement: '<PagePostsError />' },
-  'posts/[slug].tsx': { element: '<PagePost />' },
-  'posts/index.tsx': { element: '<PagePostIndex />' },
+  "posts.archive.tsx": { element: "<PagePostsArchive />" },
+  "posts/_.tsx": {
+    element: "<LayoutPosts />",
+    errorElement: "<PagePostsError />"
+  },
+  "posts/[slug].tsx": { element: "<PagePost />" },
+  "posts/index.tsx": { element: "<PagePostIndex />" }
 }
 
 export const postsPagesWithSeparation = {
-  'posts.tsx': { element: '<PagePostsArchive />' },
-  'posts/_.tsx': { element: '<LayoutPosts />', errorElement: '<PagePostsError />' },
-  'posts/[slug].tsx': { element: '<PagePost />' }
+  "posts.tsx": { element: "<PagePostsArchive />" },
+  "posts/_.tsx": {
+    element: "<LayoutPosts />",
+    errorElement: "<PagePostsError />"
+  },
+  "posts/[slug].tsx": { element: "<PagePost />" }
 }
 
 export const settingsPages = {
-  'settings.tsx': { element: '<PageSettings />' },
-  'settings/_.tsx': { element: '<LayoutSettings />' },
-  'settings/username/index.tsx': { element: '<PageUserName />' }
+  "settings.tsx": { element: "<PageSettings />" },
+  "settings/_.tsx": { element: "<LayoutSettings />" },
+  "settings/username/index.tsx": { element: "<PageUserName />" }
 }
 
 export const settingsPagesNamedIndex = {
-  'settings.tsx': { element: '<PageSettings />' },
-  'settings/_.tsx': { element: '<LayoutSettings />' },
-  'settings/username/Username.tsx': { element: '<PageUserName />' }
+  "settings.tsx": { element: "<PageSettings />" },
+  "settings/_.tsx": { element: "<LayoutSettings />" },
+  "settings/username/Username.tsx": { element: "<PageUserName />" }
 }
