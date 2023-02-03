1eac3fa7015721b2e089d6c6bb143dc6374f9ba1
Sebastian Werner <s.werner@sebastian-software.de>
2023-02-03 18:02:57 +0100

    refactor: improved docs and typings

--- src/loader.tsx
+++ src/loader.tsx
@@ -1,29 +1,43 @@
 import { lazy, Suspense } from "react";
-import type { ActionFunction, LoaderFunction, LoaderFunctionArgs, ActionFunctionArgs } from 'react-router-dom'
+import type { ActionFunction, LoaderFunction, RouteObject } from 'react-router-dom'
 
 export type ElementFactory = () => JSX.Element
 
-export type ReactRouterRouteModule = { default: ElementFactory; loader: LoaderFunction; action: ActionFunction; RouteError: ElementFactory }
+/**
+ * Defines the exports used from the actual route modules
+ */
+export type ReactRouterRouteModule = { default: ElementFactory; loader?: LoaderFunction; action?: ActionFunction;  RouteError?: ElementFactory }
+
+/**
+ * The import map retrieved from the Vite bundler maps paths to these modules.
+ */
 export type ReactRouterImportMap = Record<string, () => Promise<ReactRouterRouteModule>>
 
+/**
+ * Filter for relevant fields from official RouteObject
+ */
+export type ReactRouterBaseRouteObject = Pick<RouteObject, "element" | "errorElement" | "loader" | "action">
+
 export function modulesToLazyRouteObjects(imports: ReactRouterImportMap, root: string) {
-  const routeConfig: Record<string, BaseRoute> = {}
+  const routeConfig: Record<string, ReactRouterBaseRouteObject> = {}
   for (const fileName in imports) {
     const LazyElement = lazy(imports[fileName])
-    const LazyErrorElement = lazy(() => imports[fileName]().then((module) => ({ default: module.RouteError })))
+    const LazyErrorElement = lazy(() => imports[fileName]().then((module) => ({ default: module.RouteError ?? module.default })))
 
-    routeConfig[fileName.replace(root, "")] = {
+    const route: ReactRouterBaseRouteObject = {
       element: <Suspense fallback={null} children={<LazyElement />} />,
       errorElement: <Suspense fallback={null} children={<LazyErrorElement />} />,
-      loader: async (args: LoaderFunctionArgs) => {
+      loader: async (args) => {
         const callback = await imports[fileName]().then((module) => module.loader)
         return callback ? callback(args) : null
       },
-      action: async (args: ActionFunctionArgs) => {
+      action: async (args) => {
         const callback = await imports[fileName]().then((module) => module.action)
         return callback ? callback(args) : null
-      },
+      }
     }
+
+    routeConfig[fileName.replace(root, "")] = route
   }
 
   return routeConfig
